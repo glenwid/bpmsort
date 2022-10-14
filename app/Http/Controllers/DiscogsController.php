7cd2e3ac3153287d2b8fda6d2803c2457c6859f3
Glen Wider <glen.wider@pulsundzeit.de>
2022-10-14 18:24:31 +0200

    add route with collection access

--- app/Http/Controllers/DiscogsController.php
+++ app/Http/Controllers/DiscogsController.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class DiscogsController extends BaseController
 {
@@ -15,6 +16,20 @@ class DiscogsController extends BaseController
         # https://api.discogs.com/users/gitarristisch/collection/folders/0/releases
         # implement pagination
         # save to db
+
+        $response = Http::get('https://api.discogs.com/users/'. $username .'/collection/folders/0/releases', [
+            'token' => env('DISCOGS_TOKEN')
+        ]);
+
+        $response = json_decode($response);
+
+        $counter = 1;
+        while(property_exists($response->pagination->urls, 'next')) {
+            $counter++;
+            $response = json_Decode(Http::get($response->pagination->urls->next));
+        }
+
+        return $counter;
     }
 
     public function getTracks() {
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DiscogsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/collection/{username}', [DiscogsController::class, 'getCollection']);
+
+Route::get('/test', function(Request $request) {
+    return response('test', 200);
 });
