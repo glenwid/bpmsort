64bbc92a3128bfa4f5029166fffa34af420f03cb
Glen Wider <tcpx0152@gmail.com>
2024-11-11 18:53:18 +0100

    expose discogs user collection pull method

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\DiscogsController;
 
 Route::get('/', function () {
     return Inertia::render('Dashboard', []);
 });
+
+Route::get('/pull', [DiscogsController::class, 'pullUserCollection']);
