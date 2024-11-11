251a51facd1f04654dbb93c14583215d73e4f871
Glen Wider <tcpx0152@gmail.com>
2024-11-11 18:51:53 +0100

    add Record model and migration

---
+++ app/Models/Record.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model
+{
+    protected $fillable = [
+        'discogs_id',
+        'discogs_master_id',
+        'title',
+        'thumb',
+        'cover_image',
+    ];
+
+    /**
+     * Relations
+     */
+    public function artists()
+    {
+        return $this->belongsToMany(Artist::class);
+    }
+}
