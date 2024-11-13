5b78bf6d8a0e287eb4e20b97601a2adb34c2d648
Glen Wider <tcpx0152@gmail.com>
2024-11-13 19:07:07 +0100

    add function to convert hex color values to rgb array

---
+++ resources/js/utils.js
@@ -0,0 +1,12 @@
+export const hexToRgb = (hex) => {
+    // Remove the hash at the start if it's there
+    hex = hex.replace(/^#/, '');
+
+    // Parse the r, g, b values
+    let bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+
+    return [r, g, b];
+}
