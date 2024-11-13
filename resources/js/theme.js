af082f07d28e49a3a1fc6f14306d86110f5e48d8
Glen Wider <tcpx0152@gmail.com>
2024-11-13 19:06:38 +0100

    update color scheme

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,3 +1,4 @@
+import { Card } from "antd";
 
 export const theme = {
     colors: {
@@ -15,16 +16,39 @@ export const theme = {
             300: "#ff8500",
             400: "#ff7900",
             500: "#ff6d00",
+        },
+        black: {
+            1200: "#000000",
+            1100: "#141414", 
+            1000: "#1f1f1f",
+            900: "#262626",
+            800: '#434343', 
+            700: '#595959',
+            600: '#8c8c8c',
+            500: '#bfbfbf',
+            400: '#d9d9d9',
+            300: '#f0f0f0',
+            200: '#f5f5f5',
+            100: '#fafafa',
+        },
+        grey: {
+            500: '#746d69', 
+            400: '#acada8',
+            300: '#bbbcb6',
+            200: '#cccdc6',
+            100: '#e8e9eb',
         }
     }
 }
 
 export const antdTheme = {
     token: {
-        colorText: 'black',
-        colorPrimary: theme.colors.purple[500],
-        borderRadius: 12, 
+        colorText: theme.colors.black[1100],
+        colorPrimary: theme.colors.purple[400],
+        borderRadius: 8, 
         
-        colorBgCountainer: theme.colors.yellowrange[100],
+        colorBgContainer: theme.colors.black[100],
+        colorBorderSecondary: theme.colors.black[500],
+
     },
 };
