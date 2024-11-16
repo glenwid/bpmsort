4ac0bc04dc1b223a2aa5e31157b014755abb349c
Glen Wider <tcpx0152@gmail.com>
2024-11-16 16:06:28 +0100

    add compact AppName option

--- resources/js/Components/AppName.jsx
+++ resources/js/Components/AppName.jsx
@@ -2,17 +2,21 @@ import styled from "styled-components";
 import { Typography } from "antd";
 import { theme } from "@/theme";
 
-const StyledTitle = styled(Typography.Title)`
+const StyledTitle = styled(Typography.Title).withConfig({
+    shouldForwardProp: (prop) => !['compact'].includes(prop)
+})`
     // using :is() to increase specificity over antd
     &:is(h1) {
-        font-size: 6rem; 
+        font-size: ${props => props.compact ? '2.5rem' : '6rem'}; 
         letter-spacing: -0.02em; 
 
         color: ${theme.colors.purple[500]}; 
         margin-bottom: 0; 
+
+        flex-shrink: 0; 
     }
 `;
 
-export const AppName = () => {
-    return <StyledTitle level={1}>bpmsort</StyledTitle>
+export const AppName = ({compact}) => {
+    return <StyledTitle level={1} compact={compact}>bpmsort</StyledTitle>
 }
