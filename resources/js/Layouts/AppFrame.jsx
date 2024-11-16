fac5192d29592980a538ca067afb143cd9556728
Glen Wider <tcpx0152@gmail.com>
2024-11-16 16:40:44 +0100

    clean up AppFrame

--- resources/js/Layouts/AppFrame.jsx
+++ resources/js/Layouts/AppFrame.jsx
@@ -1,11 +1,7 @@
 import styled from "styled-components";
 import { theme } from "@/theme";
-import { AppName } from "@/Components/AppName";
 import { Feedback } from "@/Components/Feedback";
-import { Card } from "@/Components/Card";
-import { Flex, Layout as AntdLayout } from "antd";
-import { CollectionWidget } from "@/Components/CollectionWidget";
-import { usePage, useForm } from "@inertiajs/react";
+import { Layout as AntdLayout } from "antd";
 import { Header } from "@/Components/Header";
 
 export const Layout = styled(AntdLayout)`
@@ -26,41 +22,14 @@ export const Layout = styled(AntdLayout)`
         margin-left: auto;
         margin-right: auto;
     }
-
-    main {
-
-    }
 `;
 
 export const AppFrame = ({ children }) => {
-    const { records, tracks } = usePage().props;
-    const { post, processing } = useForm();
-
     return (
         <Layout>
             <Header />
 
             <Layout.Content className="content">
-                <Flex gap={12} justify="space-between">
-                    <CollectionWidget 
-                        title="Tracks in collection"
-                        count={tracks.length} 
-                        onSync={() => {
-                            post(route('tracks.sync'));
-                        }}
-                        processing={processing}
-                    />
-
-                    <CollectionWidget 
-                        title="Records in collection"
-                        count={records.length} 
-                        onSync={() => {
-                            post(route('records.sync'));
-                        }}
-                        processing={processing}
-                    />
-                </Flex>
-
                 {children}
             </Layout.Content>
 
